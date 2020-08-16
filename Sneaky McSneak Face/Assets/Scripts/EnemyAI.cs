be3c5819f2045f7aeb00c9dd0302950370d6891b
Raul Montes <montesraul7@gmail.com>
2020-08-16 15:48:52 -0700

    Added in script for AI

---
+++ Sneaky McSneak Face/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public string AIState = "Idle";
+    public float aiSenseRadius;
+    public Vector3 targetPosition;
+    public float health;
+    public float healthCutoff;
+    public float moveSpeed;
+    public float healingRate;
+    public float maxHealth;
+    public float hearingDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AIState == "Idle")
+        {
+            Idle();
+            //Check for transition
+            if (Vector3.Distance(transform.position, targetPosition) < aiSenseRadius)
+            {
+                ChangeState("Seek");
+            }
+        }
+        else if (AIState == "Seek")
+        {
+            // Do the State behavior
+            Seek();
+            // Check for transtions
+            if (health < healthCutoff)
+            {
+                ChangeState("Rest");
+            }
+            else if (Vector3.Distance(transform.position, targetPosition) >= aiSenseRadius)
+            {
+                ChangeState("Idle");
+            }
+        }
+        else if (AIState == "Rest")
+        {
+            Rest();
+            if (health >= healthCutoff)
+            {
+                ChangeState("Idle");
+            }
+        }
+        else
+        {
+            Debug.Log("AI state has not been found");
+        }
+    }
+    void Idle()
+    {
+
+    }
+    void Seek()
+    {
+        Vector3 vectorToTarget = targetPosition - transform.position;
+        transform.position += vectorToTarget.normalized * moveSpeed * Time.deltaTime;
+
+    }
+    void Rest()
+    {
+        health += healingRate * Time.deltaTime;
+        health = Mathf.Min(health, maxHealth);
+    }
+    public void ChangeState(string newState)
+    {
+        AIState = newState;
+    }
+    private bool CanHear(GameObject target)
+    {
+        // Get the target's noise maker
+        NoiseMaker targetNoiseMaker = target.GetComponent<NoiseMaker>();
+        //if they don't have a noise maker. we can't hear them.
+        if (targetNoiseMaker == null)
+        {
+            return false;
+        }
+        //If the distance between us and the target is less than the sum of the noise distance and hearing distance, we can hear i
+        float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
+        if((targetNoiseMaker.volumeDistance + hearingDistance) > distanceToTarget) { return false; }
+        return false;
+    }
+}
--- Sneaky McSneak Face/Assets/Scripts/GameManager.cs
+++ Sneaky McSneak Face/Assets/Scripts/GameManager.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+   
+    public string gameState = "Start Screen";
     public static GameManager Instance;
-    // Start is called before the first frame update
+    public GameObject titleScreen;
+    public GameObject gameUI;
+    public GameObject player;
+    public GameObject playerPf;
+    public GameObject playerPosition;
+    public GameObject playerDeathScreen;
+    public GameObject gameOverScreen;
+    public int lives = 3;
+
+        // Start is called before the first frame update
     void Start()
     {
         
@@ -27,10 +38,113 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameState == "Start Screen")
+        {
+            //do the state behavior
+            StartScreen();
+            //check transitions
+        }
+        else if (gameState == "InitializeGame")
+        {
+            //do the state behavior
+            InitializeGame();
+            //check transitions
+            ChangeState("SpawnPlayer");
+        }
+        else if (gameState == "SpawnPlayer")
+        {
+            //do the state behavior
+            SpawnPlayer();
+            //check transitions
+            ChangeState("Gameplay");
+        }
+        else if (gameState == "Gameplay")
+        {
+            //Do the state behavior
+            Gameplay();
+            //Check transitions
+            if (player == null && lives > 0)
+            {
+                ChangeState("PlayerDeath");
+            }
+            else if (player == null && lives <= 0)
+            {
+                ChangeState("GameOver");
+            }
+
+            
+        }
+        else if (gameState == "PlayerDeath")
+        {
+            //do the state behavior
+            PlayerDeath();
+            //check transitions
+            if (Input.anyKeyDown)
+            {
+                ChangeState("SpawnPlayer");
+            }
+        }
+        else if (gameState == "GameOver")
+        {
+            //do the state behavior
+            GameOver();
+            //check transitions
+            if (Input.anyKeyDown)
+            {
+                ChangeState("Start Screen");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Game manager tried to change to a non-existant state" + gameState);
+        }
+    }
+    public void ChangeState(string newState)
+    {
+        gameState = newState;
     }
     public void StartGame()
     {
         Debug.Log("Button Pressed");
+        ChangeState("Initialize Game");
+    }
+    public void StartScreen()
+    {
+        //TO show the menu.
+        if (titleScreen.activeSelf)
+        {
+            titleScreen.SetActive(true);
+        }
+    }
+    public void InitializeGame()
+    {
+        // Reset variables
+
+        //Turn off menu
+        titleScreen.SetActive(false);
+        //Turn on game UI
+        gameUI.SetActive(true);
+    }
+    public void SpawnPlayer()
+    {
+        player = Instantiate(playerPf, playerPosition.transform.position, Quaternion.identity);
+    }
+    public void Gameplay()
+    {
+
+    }
+    public void PlayerDeath()
+    {
+        if (!playerDeathScreen.activeSelf)
+        {
+            playerDeathScreen.SetActive(true);
+        }
+    }
+    public void GameOver()
+    {
+        if (!gameOverScreen.activeSelf)
+        {
+            gameOverScreen.SetActive(true);
+        }
     }
 }
