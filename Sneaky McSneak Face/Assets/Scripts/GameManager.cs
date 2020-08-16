db1e4274ba55fc65b27914006a994cc81c23464e
Raul Montes <montesraul7@gmail.com>
2020-08-16 00:17:44 -0700

    Added Text Mesh Pro and Edit Scripts
    I added the text mesh pro package into the game. Then edited mys scripts for the game manager and percentage script.

---
+++ Sneaky McSneak Face/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Debug.LogError("Game Manager tried to make a second one.");
+            Destroy(this.gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+        Debug.Log("Button Pressed");
+    }
+}
---
+++ Sneaky McSneak Face/Assets/Scripts/PercentageBar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PercentageBar : MonoBehaviour
+{
+    public float currentValue; //This is the current value of the percentage.
+    public float maxValue;  //This is the max value of the percentage.
+    private Image imageComponent; // This is where the Image Component can be called.
+    public Color barColor = Color.green; //
+    public Color barCriticalColor = Color.cyan;
+    public float criticalLevel = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Gets the objects component image. 
+        imageComponent = gameObject.GetComponent<Image>();
+        if (imageComponent != null)
+        {
+            currentValue = maxValue;
+            //This can give us the percentage of the image shown.
+            imageComponent.type = Image.Type.Filled;
+            //This sets the fill method to horizontal.
+            imageComponent.fillMethod = Image.FillMethod.Horizontal;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float percentOfMax = currentValue / maxValue;
+        imageComponent.fillAmount = percentOfMax;
+        if (percentOfMax > criticalLevel)
+        {
+            imageComponent.color = barColor;
+        }
+        else
+        {
+            imageComponent.color = barCriticalColor;
+        }
+    }
+}
